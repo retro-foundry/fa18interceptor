c552a63d4ca442a19e497d147650c348c6f755e5
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-24 10:31:30 +0100

    Reconstruct projected renderer epilogue

---
+++ source_amiga/observed/finish_projected_segment_renderer.asm
@@ -0,0 +1,5 @@
+; Byte-exact C2F47E-C2F481 linked-frame epilogue of the projected renderer.
+                org     $C2F47E
+finish_projected_segment_renderer:
+                unlk    a6
+                rts
