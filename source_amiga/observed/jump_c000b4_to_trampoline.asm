12c60eaca17af22663a94c5e0602df96eae9be6b
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-25 16:36:24 +0100

    Reconstruct local trampoline thunks

---
+++ source_amiga/observed/jump_c000b4_to_trampoline.asm
@@ -0,0 +1,8 @@
+; Byte-exact observed local dispatch thunk $C000B4-$C000B9.
+
+                org     $C000B4
+
+C06578                         equ     $C06578
+
+jump_c000b4_to_trampoline:
+                jmp     C06578.l
---
+++ source_amiga/observed/jump_c000ba_to_trampoline.asm
@@ -0,0 +1,8 @@
+; Byte-exact observed local dispatch thunk $C000BA-$C000BF.
+
+                org     $C000BA
+
+C06564                         equ     $C06564
+
+jump_c000ba_to_trampoline:
+                jmp     C06564.l
