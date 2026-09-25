db93a186c6f7abd361aa0ee605226d594fbdffe1
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-25 16:35:50 +0100

    Reconstruct local dispatch thunk

---
+++ source_amiga/observed/jump_c0004e_to_trampoline.asm
@@ -0,0 +1,8 @@
+; Byte-exact observed local dispatch thunk $C0004E-$C00053.
+
+                org     $C0004E
+
+C0655A                         equ     $C0655A
+
+jump_c0004e_to_trampoline:
+                jmp     C0655A.l
