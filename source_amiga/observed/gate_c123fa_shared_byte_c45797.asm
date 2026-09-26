840d2ab032ac3a1df1264ef45f86843ca2f3ddd8
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 10:38:47 +0100

    Reconstruct c123fa shared byte gate

---
+++ source_amiga/observed/gate_c123fa_shared_byte_c45797.asm
@@ -0,0 +1,7 @@
+; Byte-exact observed C123FA shared-byte gate $C13060-$C13067.
+
+                org     $C13060
+
+gate_c123fa_shared_byte_c45797:
+                tst.b   $C45797.l
+                beq.b   $C130AE
