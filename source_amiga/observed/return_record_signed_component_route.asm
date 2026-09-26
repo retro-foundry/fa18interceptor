36f759eae78d9565e7ca710969b50b83774079a9
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:46:07 +0100

    Reconstruct c2cc26 record component route return

---
+++ source_amiga/observed/return_record_signed_component_route.asm
@@ -0,0 +1,7 @@
+; Byte-exact record signed-component route return $C2CC26-$C2CC29.
+
+                org     $C2CC26
+
+return_record_signed_component_route:
+                unlk    a6
+                rts
