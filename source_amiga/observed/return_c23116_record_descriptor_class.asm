1193a5a201ac9b7e246e34a4e08995be5bcd2b6d
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:37:12 +0100

    Reconstruct c23116 record descriptor class return

---
+++ source_amiga/observed/return_c23116_record_descriptor_class.asm
@@ -0,0 +1,7 @@
+; Byte-exact C23116 record-descriptor class helper tail $C23170-$C23173.
+
+                org     $C23170
+
+return_c23116_record_descriptor_class:
+                movea.l (a7)+,a2
+                rts
