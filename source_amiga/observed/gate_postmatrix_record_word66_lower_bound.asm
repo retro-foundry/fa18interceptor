c4cd6b68a2b2ec76e9a21b679fe472126b226002
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:25:09 +0100

    Reconstruct postmatrix record word66 lower bound

---
+++ source_amiga/observed/gate_postmatrix_record_word66_lower_bound.asm
@@ -0,0 +1,7 @@
+; Byte-exact post-matrix record +$66 lower-bound gate $C2D80C-$C2D811.
+
+                org     $C2D80C
+
+gate_postmatrix_record_word66_lower_bound:
+                cmpi.w  #$E10,d0
+                ble.b   $C2D816
