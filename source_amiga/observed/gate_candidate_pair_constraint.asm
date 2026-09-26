4cd85fa6f2c5ccd4eff926829b95fde1e4ede99f
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:23:24 +0100

    Reconstruct candidate pair constraint gate

---
+++ source_amiga/observed/gate_candidate_pair_constraint.asm
@@ -0,0 +1,7 @@
+; Byte-exact candidate-pair constraint gate $C27410-$C27415.
+
+                org     $C27410
+
+gate_candidate_pair_constraint:
+                bsr.w   $C27456
+                beq.b   $C2744C
