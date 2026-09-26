e326c1f1bc6cce5235e2a4bd0d94bc8903478dc8
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:19:48 +0100

    Reconstruct c23116 record descriptor class1 gate

---
+++ source_amiga/observed/gate_c23116_record_descriptor_class1.asm
@@ -0,0 +1,7 @@
+; Byte-exact C23116 record-descriptor class-1 gate $C23142-$C23147.
+
+                org     $C23142
+
+gate_c23116_record_descriptor_class1:
+                cmpi.b  #1,d0
+                bne.b   $C23164
