4834ff51a6e9ddd4e285af682dfd007362ccd307
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:49:41 +0100

    Reconstruct c2e230 second matrix magnitude gate

---
+++ source_amiga/observed/gate_second_matrix_product_magnitude.asm
@@ -0,0 +1,7 @@
+; Byte-exact second matrix-product magnitude gate $C2E230-$C2E233.
+
+                org     $C2E230
+
+gate_second_matrix_product_magnitude:
+                cmp.w   d0,d6
+                bgt.b   $C2E242
