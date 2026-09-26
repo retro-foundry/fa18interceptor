aaa852e6986834bf32d7760ff286dce261350eb8
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 10:46:39 +0100

    Reconstruct indexed update shared event gate

---
+++ source_amiga/observed/gate_indexed_update_shared_event.asm
@@ -0,0 +1,7 @@
+; Byte-exact indexed-update shared-event gate $C25B70-$C25B77.
+
+                org     $C25B70
+
+gate_indexed_update_shared_event:
+                or.b    $C45788.l,d0
+                bne.b   $C25B22
