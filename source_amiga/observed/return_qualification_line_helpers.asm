200737216c1138da1b36f2db839843530d0cc2f2
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:51:29 +0100

    Reconstruct c31f48 qualification helper returns

---
+++ source_amiga/observed/return_qualification_line_helpers.asm
@@ -0,0 +1,8 @@
+; Byte-exact adjacent qualification-line helper returns $C31F48-$C31F4B.
+
+                org     $C31F48
+
+return_qualification_line_helper_a:
+                rts
+return_qualification_line_helper_b:
+                rts
