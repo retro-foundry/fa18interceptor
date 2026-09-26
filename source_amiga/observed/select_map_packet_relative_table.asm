14ce3a5caa4c339ffc88dcbf35afeb49de6a859d
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:24:28 +0100

    Reconstruct map packet relative table selection

---
+++ source_amiga/observed/select_map_packet_relative_table.asm
@@ -0,0 +1,7 @@
+; Byte-exact map-packet relative-table selection $C2AC6C-$C2AC71.
+
+                org     $C2AC6C
+
+select_map_packet_relative_table:
+                lea     $C2AA1C(pc),a0
+                bra.b   $C2AC76
