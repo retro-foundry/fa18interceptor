fa8ff631c48eed99439a56dc4f66d43c14db5ae9
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:35:12 +0100

    Reconstruct c17d6e shared pointer update return

---
+++ source_amiga/observed/return_c17d6e_shared_pointer_update.asm
@@ -0,0 +1,7 @@
+; Byte-exact C17D6E shared-pointer update epilogue $C17DA6-$C17DA9.
+
+                org     $C17DA6
+
+return_c17d6e_shared_pointer_update:
+                unlk    a6
+                rts
