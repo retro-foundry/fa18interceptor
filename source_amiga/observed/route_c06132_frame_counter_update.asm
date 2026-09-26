7a0306424a42f96cb6d9ae2675a11785284f8043
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:30:38 +0100

    Reconstruct c06132 frame counter update route

---
+++ source_amiga/observed/route_c06132_frame_counter_update.asm
@@ -0,0 +1,7 @@
+; Byte-exact C06132 frame-counter update route $C06132-$C06137.
+
+                org     $C06132
+
+route_c06132_frame_counter_update:
+                nop
+                bra.w   $C0616E
