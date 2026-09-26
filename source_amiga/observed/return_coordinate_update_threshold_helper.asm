f8211a533f5b3479edff4ba73a8361a9fdbe702e
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:33:14 +0100

    Reconstruct coordinate update threshold helper return

---
+++ source_amiga/observed/return_coordinate_update_threshold_helper.asm
@@ -0,0 +1,7 @@
+; Byte-exact coordinate-update threshold helper epilogue $C123F6-$C123F9.
+
+                org     $C123F6
+
+return_coordinate_update_threshold_helper:
+                unlk    a6
+                rts
