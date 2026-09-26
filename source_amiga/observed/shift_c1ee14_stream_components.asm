390d5d40bdf86b362f28b17c8f2ae50d74e8f8a5
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:18:03 +0100

    Reconstruct c1ee14 stream component shifts

---
+++ source_amiga/observed/shift_c1ee14_stream_components.asm
@@ -0,0 +1,8 @@
+; Byte-exact C1EE14 stream-component variable shifts $C1EF3E-$C1EF43.
+
+                org     $C1EF3E
+
+shift_c1ee14_stream_components:
+                asr.l   d3,d0
+                asr.l   d3,d1
+                asr.l   d3,d2
