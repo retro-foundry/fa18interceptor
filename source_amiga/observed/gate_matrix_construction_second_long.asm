61552a670813be9ead73c132d0a3428dc51338e6
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 10:57:31 +0100

    Reconstruct matrix construction second long gate

---
+++ source_amiga/observed/gate_matrix_construction_second_long.asm
@@ -0,0 +1,7 @@
+; Byte-exact matrix-construction second-long gate $C2E31E-$C2E325.
+
+                org     $C2E31E
+
+gate_matrix_construction_second_long:
+                tst.l   $C45BB2.l
+                bge.b   $C2E32E
