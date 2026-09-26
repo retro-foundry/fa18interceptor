8f38a08763578dc6c14a29cb39f4755f24a90597
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 10:51:35 +0100

    Reconstruct c13d84 shared long clear

---
+++ source_amiga/observed/clear_c13d84_shared_long.asm
@@ -0,0 +1,7 @@
+; Byte-exact C13D84 shared-long clear route $C15028-$C1502F.
+
+                org     $C15028
+
+clear_c13d84_shared_long:
+                clr.l   $C461C6.l
+                bra.b   $C1503C
