d21529dd224dca02191eadd6be4cef1d8efa9472
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:04:17 +0100

    Reconstruct map packet directory selector gate

---
+++ source_amiga/observed/gate_map_packet_directory_selector.asm
@@ -0,0 +1,7 @@
+; Byte-exact map-packet directory selector gate $C2AB7C-$C2AB83.
+
+                org     $C2AB7C
+
+gate_map_packet_directory_selector:
+                tst.b   $C45785.l
+                beq.b   $C2AB8C
