7b13948f7cc8059666c49f91d4b2cd389267719a
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:50:37 +0100

    Reconstruct c2e32e matrix construction d6 sign gate

---
+++ source_amiga/observed/gate_matrix_construction_d6_sign.asm
@@ -0,0 +1,7 @@
+; Byte-exact matrix-construction D6 signed gate $C2E32E-$C2E331.
+
+                org     $C2E32E
+
+gate_matrix_construction_d6_sign:
+                tst.w   d6
+                bge.b   $C2E334
