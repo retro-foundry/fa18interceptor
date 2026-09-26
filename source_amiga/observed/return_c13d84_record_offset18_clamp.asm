c217ec5745eb712fb00a5daffd72f867e54568e4
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 10:42:24 +0100

    Reconstruct c13d84 record offset18 clamp return

---
+++ source_amiga/observed/return_c13d84_record_offset18_clamp.asm
@@ -0,0 +1,8 @@
+; Byte-exact active-record +$18 clamp helper epilogue $C15130-$C15137.
+
+                org     $C15130
+
+return_c13d84_record_offset18_clamp:
+                movem.l (a7)+,d2-d4/a2-a3
+                unlk    a6
+                rts
