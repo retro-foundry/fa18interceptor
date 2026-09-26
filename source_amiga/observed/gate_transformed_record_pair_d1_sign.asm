97cc15e516494114358c98a1069cdd083063ad95
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:17:17 +0100

    Reconstruct transformed record pair d1 sign gate

---
+++ source_amiga/observed/gate_transformed_record_pair_d1_sign.asm
@@ -0,0 +1,7 @@
+; Byte-exact transformed-record pair D1 sign gate $C1EA4E-$C1EA53.
+
+                org     $C1EA4E
+
+gate_transformed_record_pair_d1_sign:
+                tst.l   d1
+                bge.w   $C1EAD8
