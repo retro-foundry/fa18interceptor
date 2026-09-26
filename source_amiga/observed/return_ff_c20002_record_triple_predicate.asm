cbbf412a57e99a23e3ab8c429dd371f450ca7b96
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:36:20 +0100

    Reconstruct c20002 record triple predicate negative return

---
+++ source_amiga/observed/return_ff_c20002_record_triple_predicate.asm
@@ -0,0 +1,7 @@
+; Byte-exact C20002 record-triple predicate negative return $C20052-$C20055.
+
+                org     $C20052
+
+return_ff_c20002_record_triple_predicate:
+                moveq   #-$1,d0
+                rts
