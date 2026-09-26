54f21794631b2a82aead5f9e8da1b9b46745f78d
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:00:30 +0100

    Reconstruct renderer counter c4583e positive gate

---
+++ source_amiga/observed/gate_renderer_counter_c4583e_positive.asm
@@ -0,0 +1,7 @@
+; Byte-exact renderer counter gate $C30B5C-$C30B63.
+
+                org     $C30B5C
+
+gate_renderer_counter_c4583e_positive:
+                tst.b   $C4583E.l
+                ble.b   $C30BC4
