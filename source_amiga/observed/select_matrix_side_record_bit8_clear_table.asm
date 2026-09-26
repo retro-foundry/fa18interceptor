0f83b2ff1eaa7bc43cc949b9c864fbd8f4543f39
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 10:41:35 +0100

    Reconstruct matrix side bit8 clear table selection

---
+++ source_amiga/observed/select_matrix_side_record_bit8_clear_table.asm
@@ -0,0 +1,6 @@
+; Byte-exact matrix-side bit-8-clear table selection $C1349A-$C134A1.
+
+                org     $C1349A
+
+select_matrix_side_record_bit8_clear_table:
+                move.l  #$C3D70C,-$6(a6)
