e4a5b7e098f32181d78c63a52b67e50dcb67f265
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 10:45:02 +0100

    Reconstruct c23228 record update magnitude classification

---
+++ source_amiga/observed/classify_c23228_record_update_magnitude.asm
@@ -0,0 +1,8 @@
+; Byte-exact C23228 record-update magnitude classification $C2323C-$C23243.
+
+                org     $C2323C
+
+classify_c23228_record_update_magnitude:
+                neg.w   d0
+                cmpi.w  #$4E,d0
+                blt.b   $C2325E
