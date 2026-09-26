82eabe788e78eb0c382667066bc2d7fff09a618c
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:18:49 +0100

    Reconstruct c1ee14 stream vector below threshold route

---
+++ source_amiga/observed/route_c1ee14_stream_vector_below_threshold.asm
@@ -0,0 +1,8 @@
+; Byte-exact C1EE14 stream-vector below-threshold route $C1EF6E-$C1EF73.
+
+                org     $C1EF6E
+
+route_c1ee14_stream_vector_below_threshold:
+                move.w  d7,d3
+                clr.w   d2
+                bra.b   $C1EF8E
