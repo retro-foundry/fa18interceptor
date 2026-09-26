b11bc0551d131db1e34e8831bc0bfd2469c979e8
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:39:29 +0100

    Reconstruct c23a7a record update class return

---
+++ source_amiga/observed/return_01_c23a7a_record_update_class.asm
@@ -0,0 +1,8 @@
+; Byte-exact default record-update class return $C23A7A-$C23A7D.
+; Class-dispatch and generic-update guard paths return status one here.
+
+                org     $C23A7A
+
+return_01_c23a7a_record_update_class:
+                moveq   #1,d0
+                rts
