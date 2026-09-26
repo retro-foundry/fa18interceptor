05c46296d011f0ead64b7f29970441b9a84c626f
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 11:15:38 +0100

    Reconstruct c123fa scaled component zero mode gate

---
+++ source_amiga/observed/gate_c123fa_scaled_component_mode_zero.asm
@@ -0,0 +1,7 @@
+; Byte-exact C123FA scaled-component zero-mode gate $C125E8-$C125ED.
+
+                org     $C125E8
+
+gate_c123fa_scaled_component_mode_zero:
+                tst.b   -$A(a6)
+                bne.b   $C125FC
