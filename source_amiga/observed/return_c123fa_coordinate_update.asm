951f1a844137b4c3689e9fd886a8f2833afbb2da
Paul Alexander Welch <paulalexanderwelch@gmail.com>
2026-09-26 10:39:33 +0100

    Reconstruct c123fa coordinate update return

---
+++ source_amiga/observed/return_c123fa_coordinate_update.asm
@@ -0,0 +1,8 @@
+; Byte-exact C123FA coordinate-update epilogue $C1316E-$C13175.
+
+                org     $C1316E
+
+return_c123fa_coordinate_update:
+                movem.l (a7)+,d2
+                unlk    a6
+                rts
